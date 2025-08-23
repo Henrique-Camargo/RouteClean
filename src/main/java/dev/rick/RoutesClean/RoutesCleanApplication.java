211d8178e0911aa752c6ff93d62883603792ca40
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-08-22 21:09:36 -0300

    Projeto RoutesClean Criado, Utilizando arquitetura limpa para um sistema de rotas de entrega

---
+++ src/main/java/dev/rick/RoutesClean/RoutesCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.rick.RoutesClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutesCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesCleanApplication.class, args);
+	}
+
+}
